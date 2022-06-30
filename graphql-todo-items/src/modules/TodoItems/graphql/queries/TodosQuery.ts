5431d1b13b79890cb4bb203790b022a55655cccb
Lukasz Gandecki <lukaszgandecki@MacBook-Pro-4.local>
2022-06-30 12:28:32 +0200

    chore: a header log for a demo

--- graphql-todo-items/src/modules/TodoItems/graphql/queries/TodosQuery.ts
+++ graphql-todo-items/src/modules/TodoItems/graphql/queries/TodosQuery.ts
@@ -1,4 +1,6 @@
 import { QueryResolvers, TodoItemDbObject } from "~generated/graphql/types";
 
-export const TodosQuery: QueryResolvers["Todos"] = (parent, args, context) =>
-  context.todoItemController.getItems() as Promise<TodoItemDbObject[]>;
+export const TodosQuery: QueryResolvers["Todos"] = (parent, args, context) => {
+  console.log("context.headers", context.headers);
+  return context.todoItemController.getItems() as Promise<TodoItemDbObject[]>;
+};
