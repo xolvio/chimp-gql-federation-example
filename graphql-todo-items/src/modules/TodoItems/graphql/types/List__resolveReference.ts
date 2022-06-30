1e30d8b2ccf53d5df21045f705d8ee8cc73a5d4f
Lukasz Gandecki <lukaszgandecki@MacBook-Pro-4.local>
2022-06-30 15:45:11 +0200

    feat: chimp updated broken

--- graphql-todo-items/src/modules/TodoItems/graphql/types/List__resolveReference.ts
+++
@@ -1,8 +0,0 @@
-import { ListResolvers } from "~generated/graphql/types";
-
-export const List__resolveReference: ListResolvers["__resolveReference"] = (
-  parent,
-  context
-) => {
-  return parent;
-};
--- graphql-todo-lists/jest.config.js
+++ graphql-todo-lists/jest.config.js
@@ -4,7 +4,6 @@ const { compilerOptions } = require("./tsconfig");
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testRunner: "jest-circus/runner",
   setupFiles: ["./jest.setup.js"],
   testPathIgnorePatterns: ["/node_modules/", "/.yalc/"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
