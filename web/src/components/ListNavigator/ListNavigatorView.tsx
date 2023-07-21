26029ded633ed65cbf51d8a64949ab764dd2069e
lgandecki <gozdak@gmail.com>
2023-07-21 11:31:41 +0200

    correctly update the incomplete count for todo lists in the frontend

--- web/src/components/ListNavigator/ListNavigatorView.tsx
+++ web/src/components/ListNavigator/ListNavigatorView.tsx
@@ -24,7 +24,7 @@ export const ListNavigatorView = ({
           activeClassName="active"
         >
           {list.todos.length ? (
-            <span className="count-list">{list.todos.length}</span>
+            <span className="count-list">{list.incompleteCount}</span>
           ) : null}
           {list.name}
         </NavLinkStyled>
